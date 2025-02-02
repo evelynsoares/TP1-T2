f38729437d2628850de0bf7bb255ca7ac6175b73
Pedro-Cardoso-UnB <117392041+Pedro-Cardoso-UnB@users.noreply.github.com>
2025-02-02 14:51:30 -0300

    terminadas as relações entre entidades

--- entidades.hpp
+++ entidades.hpp
@@ -8,31 +8,37 @@ using namespace std;
 
 ///
 ///  A Classe Conta refere-se a conta de um possível usuário
-/// Ela possui Nome, Senha e Código
+/// Ela possui Nome, Senha, Código, e Viagem
 /// @see Nome
 /// @see Senha
 /// @see Codigo
+/// @see Viagem
 /// @author 170102785
+
 ///
 
 class Conta {
 private:
     Codigo* codigo;
     Senha* senha;
+    Codigo* viagem;
 
 public:
-    Conta(Codigo* codigo, Senha* senha){
+    Conta(Codigo* codigo, Senha* senha, Codigo* viagem){
         this->senha = senha;
         this->codigo = codigo;
+        this->viagem = viagem;
     }
 
     Conta() = default;
 
     Senha getSenha() const { return *senha; }
     Codigo getCodigo() const { return *codigo;}
+    Codigo getViagem() const { return *viagem;}
 
     void setSenha(Senha& novaSenha) { *senha = novaSenha; }
     void setCodigo(Codigo& novoCodigo) { *codigo = novoCodigo;}
+    void setViagem(Codigo& codViagem) { *viagem = codViagem;}
 };
 
 ///
@@ -41,6 +47,7 @@ public:
 /// @see Codigo
 /// @see Nome
 /// @see Avaliacao
+/// @see Conta
 ///
 class Viagem {
 private:
@@ -48,6 +55,7 @@ private:
     Nome* nome;
     Avaliacao* avaliacao;
     Destino* destino;
+    vector<Codigo> contas;
 
 public:
     Viagem(Codigo* codigo, Nome* nome, Avaliacao* avaliacao, Destino* destino){
@@ -55,6 +63,7 @@ public:
         this->nome = nome;
         this->avaliacao=avaliacao;
         this->destino=destino;
+        this->contas = {};
     }
     Viagem() = default;
 
@@ -62,22 +71,27 @@ public:
     Nome getNome() const { return *nome; }
     Avaliacao getAvaliacao() const { return *avaliacao; }
     Destino getDestino() const { return *destino;}
+    vector<Codigo> getContas () const { return contas;}
 
     void setCodigo(const Codigo& novoCodigo) { *codigo = novoCodigo;}
     void setNome(const Nome& novoNome) { *nome = novoNome; }
     void setAvaliacao(const Avaliacao& novaAvaliacao) { *avaliacao = novaAvaliacao; }
     void setDestino(const Destino& novoDestino) { *destino = novoDestino;}
+    void addConta (Codigo codConta){ contas.push_back(codConta);}
+    void remConta (Codigo codConta) {contas.erase(remove(contas.begin(), contas.end(), codConta), contas.end());}
 
 };
 
 ///
 /// Destino refere-se a destinos dentro do sistema
-/// possui Codigo, Nome, duas datas (Inicio e termino), avaliação e viagens
+/// possui Codigo, Nome, duas datas (Inicio e termino), avaliação, viagens, atividade e hospedagem.
 /// @see Codigo
 /// @see Nome
 /// @see Data
 /// @see Avaliacao
-/// @see Viagens
+/// @see Viagem
+/// @see Atividade
+/// @see Hospedagem
 ///
 class Destino {
 private:
@@ -88,15 +102,18 @@ private:
     Avaliacao* avaliacao;
     vector<Codigo> viagens;
     Codigo* atividade;
+    Codigo* hospedagem;
 
 public:
-    Destino(Codigo* codigo, Nome* nome, Data* dataDeInicio, Data* dataDeTermino, Avaliacao* avaliacao){
+    Destino(Codigo* codigo, Nome* nome, Data* dataDeInicio, Data* dataDeTermino, Avaliacao* avaliacao, Codigo* atividade, Codigo* hospedagem){
         this->codigo = codigo;
         this->nome = nome;
         this->dataDeInicio = dataDeInicio;
         this->dataDeTermino = dataDeTermino;
         this->avaliacao = avaliacao;
         this->viagens = {};
+        this->atividade = atividade;
+        this->hospedagem = hospedagem;
     }
 
     Codigo getCodigo() const { return *codigo;}
@@ -106,6 +123,7 @@ public:
     Avaliacao getAvaliacao() const { return *avaliacao;}
     vector<Codigo> getViagens() const { return viagens;}
     Codigo getAtividade() const { return *atividade;}
+    Codigo getHospedagem() const { return *hospedagem;}
 
     void setCodigo( const Codigo& novoCodigo) { *codigo = novoCodigo; }
     void setNome( const Nome& novoNome) { *nome = novoNome; }
@@ -115,6 +133,7 @@ public:
     void addViagem (Codigo codViagem) { viagens.push_back(codViagem);}
     void remViagem (Codigo codViagem) {viagens.erase(remove(viagens.begin(), viagens.end(), codViagem), viagens.end());}
     void setAtividade (const Codigo& codAtividade) {*atividade = codAtividade;}
+    void setHospedagem (const Codigo& codHospedagem) { *hospedagem = codHospedagem;}
 
 };
 
@@ -135,6 +154,7 @@ private:
 ///
     Dinheiro* diaria;
     Avaliacao* avaliacao;
+    vector<Codigo> destinos;
 
 public:
     Hospedagem(Nome* nome, Codigo* codigo, Dinheiro* diaria, Avaliacao* avaliacao){
@@ -142,17 +162,21 @@ public:
         this->codigo = codigo;
         this->diaria = diaria;
         this->avaliacao = avaliacao;
+        this->destinos = {};
     }
 
     Codigo getCodigo() const { return *codigo;}
     Nome getNome() const { return *nome; }
     Dinheiro getDiaria() const { return *diaria; }
     Avaliacao getAvaliacao() const { return *avaliacao;}
+    vector<Codigo> getDestinos() const { return destinos;}
 
     void setCodigo( const Codigo& novoCodigo) { *codigo = novoCodigo; }
     void setNome( const Nome& novoNome) { *nome = novoNome; }
     void setDiaria( const Dinheiro& novaDiaria) { *diaria = novaDiaria; }
     void setAvaliacao( const Avaliacao& novaAvaliacao) { *avaliacao = novaAvaliacao; }
+    void addDestino (Codigo codDestino) { destinos.push_back(codDestino);}
+    void remDestino (Codigo codDestino) {destinos.erase(remove(destinos.begin(), destinos.end(), codDestino), destinos.end());}
 
 };
 
@@ -165,7 +189,7 @@ public:
 /// @see Duracao
 /// @see Horario
 /// @see Avaliacao
-/// @see Destinos
+/// @see Destino
 ///
 class Atividade {
 private:
